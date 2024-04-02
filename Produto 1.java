1320d3ab16a2a2250d0edd0b6a67ca7f48413440
mclaracsoares <133005094+mclaracsoares@users.noreply.github.com>
2024-04-02 10:31:31 -0300

    Update Produto 1.java

--- Produto 1.java
+++ Produto 1.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
+// Representa um produto com nome, quantidade e preço.
 class Produto {
-    private String nome;
-    private int quantidade;
-    private double preco;
+    private String nome; // Nome do produto
+    private int quantidade; // Quantidade 
+    private double preco; // Preço 
 
+    // Construtor da classe Produto
     public Produto(String nome, int quantidade, double preco) {
         this.nome = nome;
         this.quantidade = quantidade;
         this.preco = preco;
     }
+
+    // Getters e setters para acessar e modificar os atributos do produto
 
     public String getNome() {
         return nome;
@@ -37,6 +40,7 @@ class Produto {
         this.preco = preco;
     }
 
+    // Retorna uma representação textual do objeto Produto
     @Override
     public String toString() {
         return "Nome: " + nome + ", Quantidade: " + quantidade + ", Preço: R$" + preco;
